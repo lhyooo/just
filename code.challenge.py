c14f8d32251d695ddf092399ef84eb9a4182aabc
lhyooo <gksdud1220242@naver.com>
2024-05-30 14:51:44 +0900

    240530

---
+++ code.challenge.py
@@ -0,0 +1,8 @@
+JUST = ["2de", 3, True, "w3", {"school": "kdmhs"}]
+kdmhs = [ "cx", JUST[:1], 2, JUST[3][0]] 
+JUST.insert(2, 5) 
+findf = [21, kdmhs[1][0]] 
+findf.pop(0)
+del kdmhs[0:3] 
+result = JUST[-5], findf[0][2], int(bool("")), kdmhs[0], int(bool(0))
+print(result)
